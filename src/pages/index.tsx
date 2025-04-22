c1aad073704662d2d82603450c4e47fc4e3404a6
diogo-lima-flooxai <diogo.lima.flooxai@gmail.com>
2025-04-22 09:16:22 -0300

    feat: add table component, with edit and delete functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
 import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Eli', 42, '1'),
+    new Cliente('Bia', 40, '2'),
+    new Cliente('Ana', 32, '3'),
+    new Cliente('Pedro', 22, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+
+  }
+
+  function clienteExcluido(cliente: Cliente){
+
+  }
+
   return (
     <div className={`
     flex justify-center items-center h-screen
@@ -8,7 +26,9 @@ export default function Home() {
     text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes} 
+        clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido} />
       </Layout>
     </div>
   )
